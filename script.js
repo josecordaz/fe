fa0adbbcc08f365c0fffbd14ef99221567d262f3
Jose Carlos Ordaz <josecordaz@gmail.com>
2019-11-09 07:49:00 -0600

    fixed js patterns

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const Http = new XMLHttpRequest();
-const url='http://localhost:8283';
+const url="http://localhost:8283";
 Http.open("GET", url);
 Http.send();
 
 Http.onreadystatechange = (e) => {
     if(Http.readyState == 4 && Http.status == 200) {
-        const response = Http.responseText
-        person = JSON.parse(response)
-        addInfo(person)
+        const response = Http.responseText;
+        var person = JSON.parse(response);
+        addInfo(person);
     }
-}
+};
 
 function addInfo(person){
-    var div = document.createElement("h2")
-    div.innerHTML = person.name + ", " + person.email
-    document.body.append(div)
-}
+    var div = document.createElement("h2");
+    div.innerHTML = person.name + ", " + person.email;
+    document.body.append(div);
+};
