bff2e09364586d1fa3e600e07ada357bc8a48453
Jose Carlos Ordaz <josecordaz@gmail.com>
2019-11-09 07:53:41 -0600

    fixed security and error prone

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const url="http://localhost:8283";
 Http.open("GET", url);
 Http.send();
 
+function addInfo(person){
+    var div = document.createElement("h2");
+    div.textContent = person.name + ", " + person.email;
+    document.body.append(div);
+};
+
 Http.onreadystatechange = (e) => {
-    if(Http.readyState == 4 && Http.status == 200) {
+    if(Http.readyState === 4 && Http.status === 200) {
         const response = Http.responseText;
         var person = JSON.parse(response);
         addInfo(person);
     }
 };
 
-function addInfo(person){
-    var div = document.createElement("h2");
-    div.innerHTML = person.name + ", " + person.email;
-    document.body.append(div);
-};
