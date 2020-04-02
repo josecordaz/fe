8c7f13ae69411ab1da60e52d27b4b55eea534513
J Carlos Ordaz <josecordaz@gmail.com>
2020-04-02 11:45:59 -0700

    use onload instead

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const Http = new XMLHttpRequest();
-const url="http://localhost:8283";
-Http.open("GET", url);
-Http.send();
-
-var addInfo = (person) => {
-    var div = document.createElement("h2");
-    div.textContent = person.name + ", " + person.email;
-    document.body.append(div);
-}
-
-Http.onreadystatechange = (e) => {
-    if(Http.readyState === 4 && Http.status === 200) {
-        const response = Http.responseText;
-        var person = JSON.parse(response);
-        addInfo(person);
+function init(){
+    const Http = new XMLHttpRequest();
+    const url="http://localhost:8283";
+    Http.open("GET", url);
+    Http.send();
+    
+    var addInfo = (person) => {
+        var div = document.createElement("h2");
+        div.textContent = person.name + ", " + person.email;
+        document.body.append(div);
+    }
+    
+    Http.onreadystatechange = (e) => {
+        if(Http.readyState === 4 && Http.status === 200) {
+            const response = Http.responseText;
+            var person = JSON.parse(response);
+            addInfo(person);
+        }
     }
 }
